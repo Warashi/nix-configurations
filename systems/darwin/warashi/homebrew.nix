5acbfc87d45ca24278c44a04690c16293b45c5e5
Shinnosuke Sawada <shin@warashi.dev>
2025-11-23 08:07:46 +0900

    chore: update homebrew packages for warashi

--- systems/darwin/warashi/homebrew.nix
+++ systems/darwin/warashi/homebrew.nix
@@ -1,37 +1,22 @@
 {
   homebrew = {
     enable = true;
-    brews = [ ];
+    brews = [
+      "mas"
+      "mint"
+    ];
     casks = [
-      "android-studio"
       "1password"
-      "chatgpt"
-      "claude"
-      "cryptomator"
-      "cursor"
-      "discord"
       "domzilla-caffeine"
-      "dropbox"
-      "firefox"
       "ghostty"
-      "google-chrome"
-      "grammarly-desktop"
       "karabiner-elements"
-      "logi-options+"
       "microsoft-edge"
-      "orion"
       "raycast"
-      "realforce"
-      "slack"
       "tailscale-app"
-      "visual-studio-code"
-      "zoom"
     ];
     masApps = {
-      # DaisyDisk = 411643860;
-      # "辞書 by 物書堂 " = 1380563956;
-      # Pastebot = 1179623856;
-      # TickTick = 966085870;
+      DaisyDisk = 411643860;
+      Pastebot = 1179623856;
     };
     onActivation = {
       cleanup = "zap";
