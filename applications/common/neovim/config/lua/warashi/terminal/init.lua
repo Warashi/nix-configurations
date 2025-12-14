db9a2ad3f6ebe4829c0ba28b19f754334463140c
Shinnosuke Sawada <shin@warashi.dev>
2025-12-14 20:42:38 +0900

    feat(nvim): add OSC 7 support for terminal cwd changes

--- applications/common/neovim/config/lua/warashi/terminal/init.lua
+++ applications/common/neovim/config/lua/warashi/terminal/init.lua
@@ -1,1 +1,20 @@
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd({ "TermRequest" }, {
+	desc = "Handles OSC 7 dir change requests",
+	callback = function(ev)
+		local val, n = string.gsub(ev.data.sequence, "\027]7;file://[^/]*", "")
+		if n > 0 then
+			-- OSC 7: dir-change
+			local dir = val
+			if vim.fn.isdirectory(dir) == 0 then
+				vim.notify("invalid dir: " .. dir)
+				return
+			end
+			vim.b[ev.buf].osc7_dir = dir
+			if vim.api.nvim_get_current_buf() == ev.buf then
+				vim.cmd.tcd(dir)
+			end
+		end
+	end,
+})
