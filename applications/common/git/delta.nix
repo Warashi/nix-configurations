367eee55a254c355f1b1bb5a2556d2e524aab458
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-12-24 10:00:54 +0900

    feat(delta): use ov as pager for git integration

--- applications/common/git/delta.nix
+++ applications/common/git/delta.nix
@@ -1,10 +1,40 @@
 {
-  programs.delta = {
-    enable = true;
-    enableGitIntegration = true;
-    options = {
-      line-numbers = true;
-      side-by-side = true;
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  ov = lib.getExe' pkgs.ov "ov";
+  delta = lib.getExe' config.programs.delta.finalPackage "delta";
+  catppuccin-feature = "catppuccin-${config.catppuccin.delta.flavor}";
+in
+{
+  programs = {
+    git.iniContent = {
+      core.pager = lib.mkForce "${delta} --pager='${ov} --quit-if-one-screen'";
+      pager = {
+        show = "${delta} --pager='${ov} --quit-if-one-screen --header 3'";
+        diff = "${delta} --features ov-diff";
+        log = "${delta} --features ov-log";
+      };
+    };
+    delta = {
+      enable = true;
+      enableGitIntegration = true;
+      options = {
+        line-numbers = true;
+        navigate = true;
+        side-by-side = true;
+        ov-diff = {
+          features = catppuccin-feature;
+          pager = "${ov} --quit-if-one-screen --section-delimiter '^(commit|added:|removed:|renamed:|Δ)' --section-header --pattern '•'";
+        };
+        ov-log = {
+          features = catppuccin-feature;
+          pager = "${ov} --quit-if-one-screen --section-delimiter '^commit' --section-header-num 3";
+        };
+      };
     };
   };
 }
