47476940db431e65762f4c2443b3bd9ec35bcd16
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-12-26 15:18:03 +0900

    feat: add claude-code settings merger and overrides

--- applications/common/ai-tools/claude-code.nix
+++ applications/common/ai-tools/claude-code.nix
@@ -2,12 +2,38 @@
   config,
   inputs,
   pkgs,
+  lib,
   ...
 }:
+let
+  merger = pkgs.callPackage ./merger { };
+  config-overrides = (pkgs.formats.json { }).generate "claude-settings-override.json" {
+    env = {
+      ANTHROPIC_MODEL = "opusplan";
+    };
+    permissions = {
+      defaultMode = "plan";
+    };
+    alwaysThinkingEnabled = true;
+    enabledPlugins = {
+      "gopls-lsp@claude-plugins-official" = true;
+    };
+  };
+in
 {
   home = {
     file = {
       ".claude/CLAUDE.md".source = ./AGENTS.md;
+    };
+    activation = {
+      warashi-claude-code-config-merger = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        if [ ! -f ${config.home.homeDirectory}/.claude/settings.json ]; then
+          echo "No existing claude-code config found, skipping merge."
+          exit 0
+        fi
+        run mv ${config.home.homeDirectory}/.claude/settings.json ${config.home.homeDirectory}/.claude/settings.backup.json
+        run ${lib.getExe merger} ${config.home.homeDirectory}/.claude/settings.json ${config.home.homeDirectory}/.claude/settings.backup.json ${config-overrides}
+      '';
     };
   };
 }
