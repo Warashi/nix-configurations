90abbf979a19db83a5dfa765148c3c03dfa79d21
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2026-01-01 07:14:36 +0900

    fix: moonbit treesitter grammar and filetype detection

--- applications/common/neovim/moonbit-treesitter-grammar.nix
+++ applications/common/neovim/moonbit-treesitter-grammar.nix
@@ -1,8 +1,12 @@
 {
   tree-sitter,
+  symlinkJoin,
   fetchFromGitHub,
+  vimUtils,
+  neovimUtils,
+  runCommand,
 }:
-tree-sitter.buildGrammar {
+let
   language = "moonbit";
   version = "0.1.0";
   src = fetchFromGitHub {
@@ -11,4 +15,28 @@ tree-sitter.buildGrammar {
     rev = "a5a7e0b9cb2db740cfcc4232b2f16493b42a0c82";
     hash = "sha256-CMI+qSZ/91e/YpFJYtczOGdaRAOfhZiZRFgdPiq1jJ8=";
   };
+
+  queries = vimUtils.toVimPlugin (
+    runCommand "nvim-treesitter-queries-moonbit"
+      {
+        passthru = { inherit language; };
+        meta.description = "Queries for ${language} from nvim-treesitter";
+
+      }
+      ''
+        mkdir -p "$out/queries/moonbit"
+        cp ${src}/queries/* "$out/queries/moonbit/"
+      ''
+  );
+  grammer = tree-sitter.buildGrammar {
+    inherit language version src;
+  };
+  grammarPlugin = neovimUtils.grammarToPlugin grammer;
+in
+symlinkJoin {
+  name = "nvim-treesitter-grammar-moonbit";
+  paths = [
+    grammarPlugin
+    queries
+  ];
 }
