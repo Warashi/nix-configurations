f684eb18770f03196702dec4ccbaeb3237943f30
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2026-01-01 07:24:07 +0900

    feat: enable treesitter always

--- applications/common/neovim/config/init.lua
+++ applications/common/neovim/config/init.lua
@@ -2,10 +2,13 @@ vim.o.clipboard = "unnamedplus"
 vim.opt.runtimepath:prepend("@merged_plugins@")
 vim.opt.termguicolors = true
 
+-- keep-sorted start
 require("warashi.aibo")
 require("warashi.ddc")
 require("warashi.lsp")
 require("warashi.skkeleton")
 require("warashi.terminal")
+require("warashi.treesitter")
+-- keep-sorted end
 
 vim.cmd.colorscheme("retrobox")
