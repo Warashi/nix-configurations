b507daaf82de24543f41fbed883f9d97176f8bf1
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2026-01-01 07:40:59 +0900

    feat(nvim): add skkbit to runtimepath

--- applications/common/neovim/config/init.lua
+++ applications/common/neovim/config/init.lua
@@ -1,5 +1,6 @@
 vim.o.clipboard = "unnamedplus"
 vim.opt.runtimepath:prepend("@merged_plugins@")
+vim.opt.runtimepath:prepend(vim.env.HOME .. "/ghq/github.com/Warashi/skkbit")
 vim.opt.termguicolors = true
 
 -- keep-sorted start
