4583b01c33d4a96ec369001ba72f4b98e48fe6e9
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2026-01-02 10:51:45 +0900

    feat(nvim): open loclist for diagnostics

--- applications/common/neovim/config/lua/warashi/lsp/init.lua
+++ applications/common/neovim/config/lua/warashi/lsp/init.lua
@@ -10,3 +10,20 @@ vim.lsp.enable("sourcekit")
 
 vim.fn["signature_help#enable"]()
 vim.fn["popup_preview#enable"]()
+
+vim.diagnostic.handlers.loclist = {
+	show = function(_, _, _, opts)
+		-- Generally don't want it to open on every update
+		opts.loclist.open = opts.loclist.open or false
+		local winid = vim.api.nvim_get_current_win()
+		vim.diagnostic.setloclist(opts.loclist)
+		vim.api.nvim_set_current_win(winid)
+	end,
+}
+
+vim.diagnostic.config({
+	loclist = {
+		open = true,
+		severity = { min = vim.diagnostic.severity.HINT },
+	},
+})
