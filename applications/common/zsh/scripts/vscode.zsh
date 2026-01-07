fb797321f71835df57752de9fdb15b0a102e7c87
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2026-01-07 19:43:12 +0900

    feat: use agy for VSCode shell integration if available

--- applications/common/zsh/scripts/vscode.zsh
+++ applications/common/zsh/scripts/vscode.zsh
@@ -1,7 +1,9 @@
 if [[ "$TERM_PROGRAM" == "vscode" ]]; then
-    if which cursor &>/dev/null; then
-        . "$(cursor --locate-shell-integration-path zsh)"
+    if which agy &>/dev/null; then
+        . "$(agy --locate-shell-integration-path zsh)"
     elif which code &>/dev/null; then
         . "$(code --locate-shell-integration-path zsh)"
+    elif which cursor &>/dev/null; then
+        . "$(cursor --locate-shell-integration-path zsh)"
     fi
 fi
