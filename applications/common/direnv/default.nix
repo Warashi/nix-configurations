6aa7e5f1fcee1c4ffb40f6b780113e8702f6215e
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2026-02-10 12:25:00 +0900

    fix(direnv): run preInstall and postInstall

--- applications/common/direnv/default.nix
+++ applications/common/direnv/default.nix
@@ -1,7 +1,15 @@
+{ pkgs, ... }:
 {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    package = pkgs.direnv.overrideAttrs (oldAttrs: {
+      installPhase = ''
+        runHook preInstall
+        ${oldAttrs.installPhase}
+        runHook postInstall
+      '';
+    });
   };
   programs.direnv-instant = {
     enable = true;
