365505808cbe846d77629ef777ab5e7cd8dc5db9
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2026-02-28 09:01:04 +0900

    feat(go): use latest

--- applications/common/go/default.nix
+++ applications/common/go/default.nix
@@ -1,7 +1,8 @@
-{ lib, ... }:
+{ pkgs, ... }:
 {
   programs.go = {
     enable = true;
+    package = pkgs.go_latest;
     env = {
       GOCACHE = "/tmp/go-cache/build";
       GOMODCACHE = "/tmp/go-cache/mod";
