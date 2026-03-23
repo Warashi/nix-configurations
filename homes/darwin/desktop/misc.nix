5a1ec84c08978acb7fc910ac103e7ba154cd8b62
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2026-03-24 05:41:09 +0900

    feat(darwin): add 1password ssh agent socket to session variables

--- homes/darwin/desktop/misc.nix
+++ homes/darwin/desktop/misc.nix
@@ -1,7 +1,8 @@
 {
+  config,
   pkgs,
-  specialArgs,
   lib,
+  specialArgs,
   ...
 }:
 let
@@ -11,6 +12,10 @@ in
   warashi.services.muscat.enable = true;
 
   home = {
+    sessionVariables = {
+      SSH_AUTH_SOCK = "${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+    };
+
     packages = with pkgs; [
       terminal-notifier
     ];
